c7f287afef44157c0bcd412b6de5c42faf8a2844
Mihail Shukin <mihailshukin@yandex.ru>
2021-12-15 20:12:47 +0300

    app

---
+++ Pages/hire.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcoreap.Pages
+{
+    public class hireModel : PageModel
+    {
+        private readonly ILogger<hireModel> _logger;
+
+        public hireModel(ILogger<hireModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
